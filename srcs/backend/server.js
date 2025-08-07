e71340e551c6fa766e61d908e89930fa441a865e
Marsha <151203981+marsha-t@users.noreply.github.com>
2025-08-07 11:27:55 +0000

    add basic backend container

---
+++ srcs/backend/server.js
@@ -0,0 +1,20 @@
+// server.js
+import Fastify from 'fastify';
+
+const app = Fastify({ logger: true });
+
+app.get('/ping', async (request, reply) => {
+  return { msg: 'pong' };
+});
+
+const start = async () => {
+  try {
+    await app.listen({ port: 3000, host: '0.0.0.0' });
+    console.log('Server running at http://localhost:3000');
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
