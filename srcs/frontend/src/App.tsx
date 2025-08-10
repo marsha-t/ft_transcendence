b6fb8c1640756bfdf56999b4069b72fe0fcfae36
mk-sabira <buusabira.makhamatkayimkyzy.ch22@gmail.com>
2025-08-10 10:52:57 +0400

    added basic frontend conatainer setting

---
+++ srcs/frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to ft_transcendence frontend!</h1>
+    </div>
+  );
+};
+
+export default App;
