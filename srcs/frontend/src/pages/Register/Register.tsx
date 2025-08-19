260bc4e0736b88bab1aec6a71ef1dd4c9c9c8f99
mk-sabira <buusabira.makhamatkayimkyzy.ch22@gmail.com>
2025-08-19 11:16:30 +0400

    frontend of the register page is done

--- srcs/frontend/src/pages/Register/Register.tsx
+++ srcs/frontend/src/pages/Register/Register.tsx
@@ -4,8 +4,8 @@ import "./Register.css"
 const Register: React.FC = () => {
   return (
     <div className="register_page">
+      <h1  className="register_title">Create an account</h1>
       <div className="register_block">
-        <h1>Create Account</h1>
         <form className="register_form">
           <div>
             <label htmlFor="nickname">Nickname</label>
@@ -13,13 +13,16 @@ const Register: React.FC = () => {
           </div>
           <div>
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" placeholder="Email"/>
+            <input type="email" id="email" placeholder="example@email.com"/>
           </div>
           <div>
             <label htmlFor="password">Email</label>
             <input type="password" id="password" placeholder="Password"/>
           </div>
-          <button type="submit">Register</button>
+          <button type="submit">Create an account</button>
+          <p className="login_text">Already have an account?
+            <a href="/login">Login</a>
+          </p>
         </form>
       </div>
     </div>
