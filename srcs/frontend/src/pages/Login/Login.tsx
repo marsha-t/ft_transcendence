8b2fc3e1d1b4f68978d1c6aa59c566728d3ffbed
mk-sabira <buusabira.makhamatkayimkyzy.ch22@gmail.com>
2025-08-20 19:07:36 +0400

    added login frontend page's css

--- srcs/frontend/src/pages/Login/Login.tsx
+++ srcs/frontend/src/pages/Login/Login.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import "./Login.css"
 
 const Login: React.FC = () => {
-  return <header>Login Content</header>;
+  return (
+    <div className="login_page">
+      <h1 className="login_title">Welcome Back</h1>
+      <div className="login_block">
+        <form className="login_form">
+          <p className="register_text">Don’t have an account yet?
+            <a href="/register">Register</a>
+          </p>
+          <div>
+            <label htmlFor="nickname">Nickname</label>
+            <input type="text" id="nickname" placeholder="Nickname"/>
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" placeholder="Password"></input>
+          </div>
+          <button type="submit">Login</button>
+        </form>
+
+      </div>
+
+    </div>
+  )
 };
 
 export default Login;
