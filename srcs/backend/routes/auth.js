8bcbda12b1d3052d7350fa2d4802f0edad4b3b43
Rawan Al Jaberi <154672950+raljaberi@users.noreply.github.com>
2025-08-21 13:47:44 +0000

    Changed validation from manual to JSON schema

--- srcs/backend/routes/auth.js
+++ srcs/backend/routes/auth.js
@@ -1,45 +1,14 @@
 // routes/auth.js
 
+import { registerSchema } from '../schemas/auth.js';
+
 const users = []; // this is our temporary fake database
 
 async function authRoutes(app, options) {
-  app.post('/api/register', async (request, reply) => {
+  app.post('/api/register', { schema: registerSchema }, async (request, reply) => {
     const { username, email, password } = request.body;
 
-    // 1- Check input
-    if (!username || !email || !password) {
-      return reply.code(400).send({ 
-        error: 'Username, email, and password are required' });
-    }
-
-    // 2- Validate username
-    const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
-
-    if (!usernameRegex.test(username)) {
-      return reply.code(400).send({
-        error: 'Username must be 3-20 characters and contain only letters, numbers, or underscores'
-      });
-    }
-
-    // 3- Validate password complexity
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{12,}$/;
-
-    if (!passwordRegex.test(password)) {
-      return reply.code(400).send({
-        error: 'Password must be at least 12 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character.'
-      });
-    }
-
-    // 4- Validate email syntax
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (!emailRegex.test(email)) {
-      return reply.code(400).send({ 
-        error: 'Invalid email format' 
-      });
-    }
-
-    // 5- Check if username already exists
+    // Check if username already exists
     const userExists = users.find(
       (user) => user.username === username
     );
@@ -48,7 +17,7 @@ async function authRoutes(app, options) {
       return reply.code(409).send({ error: 'Username already exists' });
     }
 
-    // 6- Check if email already exists
+    // Check if email already exists
     const emailExists = users.find(
       (user) => user.email === email
     );
@@ -57,7 +26,7 @@ async function authRoutes(app, options) {
       return reply.code(409).send({ error: 'Email already exists' });
     }
 
-    // 7- Store user
+    // Store user
     users.push({ username, email, password });
 
     return reply.code(201).send({ message: 'User registered successfully' });
