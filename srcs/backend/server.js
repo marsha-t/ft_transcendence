8fbbd2bcd34bda3e4a4b2530a72c038bc23ef9a3
Rawan Al Jaberi <154672950+raljaberi@users.noreply.github.com>
2025-08-21 13:47:44 +0000

    Changed validation from manual to JSON schema

--- srcs/backend/server.js
+++ srcs/backend/server.js
@@ -2,8 +2,36 @@
 
 import Fastify from 'fastify';
 import authRoutes from './routes/auth.js'; // import auth routes
+import swagger from '@fastify/swagger';
+import swaggerUi from '@fastify/swagger-ui';
 
 const app = Fastify({ logger: true });
+
+// Swagger ------------------------------------------
+await app.register(swagger, {
+    openapi: {
+      info: {
+        title: 'ft_transcendence Backend',
+        description: 'API docs for our backend routes',
+        version: '1.0.0',
+      },
+      servers: [
+        { url: 'https://congenial-space-winner-x5544j4rjr4ph9v9r-5000.app.github.dev/' },
+      ],
+    },
+  });
+
+  await app.register(swaggerUi, {
+    routePrefix: '/docs',
+    uiConfig: {
+      docExpansion: 'full',
+      deepLinking: false,
+    },
+  });
+
+  // Register your routes
+  // app.register(authRoutes); ???????????
+// ------------------------------------------------------
 
 // Test route
 app.get('/api/ping', async (request, reply) => {
