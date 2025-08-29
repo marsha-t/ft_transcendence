9385abd21a6e32912cf5543c3ca5f5a72d0fbfff
mk-sabira <buusabira.makhamatkayimkyzy.ch22@gmail.com>
2025-08-29 12:38:46 +0400

    added header and with its css file

--- srcs/frontend/src/components/Header.ts
+++ srcs/frontend/src/components/Header.ts
@@ -1,35 +1,58 @@
 import { IComponent } from "./IComponent";
 
-export class Header implements IComponent {
+export class Header implements IComponent{
+    
     public render(): HTMLElement {
         const header = document.createElement('header');
+        header.className = 'header';
+
         const nav = document.createElement('nav');
+        nav.className = 'navbar';
+
+        //logo
         const logo = document.createElement('a');
-        logo.href = '#main';
-        logo.textContent = 'Transcendence';
+        logo.href = 'main';
+        logo.className = 'logo';
+
+        //logo has two elements 1) text
+        const logoText = document.createElement('a');
+        logoText.className = 'logo_text';
+        logoText.textContent = 'PONG';
+
+        //logo icon
+        const logoIcon = document.createElement('img');
+        logoIcon.src = '/assets/logo.png'; 
+        logoIcon.alt = 'Pong Logo';
+        logoIcon.className = 'logo_icon';
+
+
+        //Navigation links
         const navLinks = document.createElement('ul');
+        navLinks.className = 'nav_links';
 
         const links = [
-            { text: 'Main', href: 'main' },
-            { text: 'About', href: 'about' },
-            { text: 'Game', href: 'game' },
-            { text: 'Login', href: 'login' },
-            { text: 'Register', href: 'register' }
+            {text: 'Home', href: '/main'},
+            {text: 'Creators', href: '/about'},
+            {text: 'Login', href: '/login'},
+            {text: 'Register', href: '/register'},
         ];
 
         links.forEach(link => {
             const li = document.createElement('li');
             const a = document.createElement('a');
+
             a.href = link.href;
             a.textContent = link.text;
             li.appendChild(a);
             navLinks.appendChild(li);
         });
 
+        logo.appendChild(logoIcon);
+        logo.appendChild(logoText);
+
         nav.appendChild(logo);
         nav.appendChild(navLinks);
         header.appendChild(nav);
-
         return header;
     }
 }
