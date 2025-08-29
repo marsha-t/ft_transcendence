f2aff8afe8df61b09231235b723a9ae2e0175e2e
mk-sabira <buusabira.makhamatkayimkyzy.ch22@gmail.com>
2025-08-29 12:38:46 +0400

    added header and with its css file

--- srcs/frontend/src/components/IComponent.ts
+++ srcs/frontend/src/components/IComponent.ts
@@ -1,12 +1,4 @@
-/**
- * Defines the contract for all components and pages in the application.
- * Any class that implements this interface must have a 'render' method
- * that returns a single HTMLElement to be displayed on the page.
- */
+
 export interface IComponent {
-    /**
-     * Renders the component or page.
-     * @returns The root HTMLElement of the component.
-     */
     render(): HTMLElement;
 }
