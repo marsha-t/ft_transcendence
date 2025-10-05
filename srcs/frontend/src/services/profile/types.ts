ef0d79b6227f083de9a4cb8e7057fdaec3dab7a2
Rawan Al Jaberi <154672950+raljaberi@users.noreply.github.com>
2025-10-05 09:02:39 +0000

    Updating avatar (delete & upload) connected

--- srcs/frontend/src/services/profile/types.ts
+++ srcs/frontend/src/services/profile/types.ts
@@ -8,6 +8,17 @@ export interface FriendsData {
     friends?: { avatarURL: string; name: string; online: boolean }[];
 }
 
+export interface AvatarUploadResponse {
+    message: string;
+    avatar: string; // the new avatar URL returned by backend
+}
+
+export interface AvatarDeleteResponse {
+    message: string;
+    avatar: string;
+}
+  
+
 export interface ApiResponse<T> {
     success: boolean;
     data?: T;
