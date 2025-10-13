884de38b04cbb9283dea87a9228854df38bf32f8
mk-sabira <buusabira.makhamatkayimkyzy.ch22@gmail.com>
2025-10-13 12:10:05 +0400

    added tailwind css and new style to the main and creators pages

--- srcs/frontend/src/pages/Creators.ts
+++ srcs/frontend/src/pages/Creators.ts
@@ -3,21 +3,34 @@ import { IComponent } from "../components/IComponent";
 export class Creators implements IComponent {
     public render(): HTMLElement {
         const container = document.createElement('div');
-        container.className = "creators_page";
+        container.className = `flex justify-center
+            flex-wrap min-h-[85vh] py-[23px] 
+            bg-color-yellow`;
 
-        // this.loadPageStyles();
-
+        // === SUBcontainer ===
+        const subContainer = document.createElement('div');
+        subContainer.className = `
+            flex flex-row items-center justify-between
+            flex-wrap bg-background
+            rounded-[16px] shadow-lg
+            mx-[23px] w-[calc(100%-46px)]
+            h-auto p-10 gap-14`;
+            
         //left half
         const text_section = document.createElement('div');
-        text_section.className = "text_section";
+        text_section.className = `flex-1 basis-[400px] max-w-[600px]
+            text-color_white font-press `;
 
         const heading = document.createElement('h1');
-        heading.className = "main_title";
+        heading.className = `text-[clamp(18px,4vw,28px)]
+            mb-4 tracking-widest`;
         heading.textContent = "Creators";
         text_section.appendChild(heading);
 
         const description = document.createElement('p');
-        description.className = "description";
+        description.className = `text-[clamp(8px,1.2vw,10px)] 
+            leading-[1.5]font-press text-[10px] 
+            leading-[3] tracking-wider`;
         description.textContent = `Contrary to popular belief, Lorem Ipsum is not simply 
                                     random text. It has roots in a piece of classical Latin 
                                     literature from 45 BC, making it over 2000 years old. Richard 
@@ -34,91 +47,100 @@ export class Creators implements IComponent {
 
         text_section.appendChild(description);
 
-        //avatar_section
+        // Update the image_section
         const image_section = document.createElement('div');
-        image_section.className = "image_section"
+        image_section.className = `flex-1 flex justify-center 
+            font-press text-color_white ml-20`;
 
         const avatarsGrid = document.createElement('div');
-        avatarsGrid.className = "avatars_grid"
+        avatarsGrid.className = `grid grid-cols-2 gap-[40px] mt-[20px]`;
 
-        //firts avatar
+        // First avatar
         const firtsAvatar = document.createElement("div");
-        firtsAvatar.className = "avatar_card";
+        firtsAvatar.className = `flex flex-col items-start 
+            p-4 border-2 border-red-500 rounded-[10px]
+            w-[180px] h-[200px]`;
 
         const avatar_marsha = document.createElement('img');
         avatar_marsha.src = "/assets/avatar/marsha.png";
         avatar_marsha.alt = "avatar marsha";
-        avatar_marsha.className = 'avatar_img';
+        avatar_marsha.className = `w-[120px] h-[120px] rounded-full object-cover mb-2 self-center`;
 
-        const marsha = document.createElement('a');
-        marsha.className = "avatar_name";
+        const marsha = document.createElement('p');
+        marsha.className = "text-[8px] font-bold text-left w-full mb-1 mt-2";
         marsha.textContent = "Marsha Teo";
 
-        const role_marsha = document.createElement('a');
-        role_marsha.className = "avatar_role";
+        const role_marsha = document.createElement('p');
+        role_marsha.className = `text-[8px] text-left w-full`;
         role_marsha.textContent = "Backend developer";
 
         firtsAvatar.appendChild(avatar_marsha);
         firtsAvatar.appendChild(marsha);
         firtsAvatar.appendChild(role_marsha);
-        
-        //second avatar
+
+        // Second avatar
         const secondAvatar = document.createElement("div");
-        secondAvatar.className = "avatar_card";
+        secondAvatar.className =  `flex flex-col items-start 
+            p-4 border-2 border-red-500 rounded-[10px]
+            w-[180px] h-[200px]`;
 
         const avatar_sabira = document.createElement('img');
         avatar_sabira.src = "/assets/avatar/sabira.png";
         avatar_sabira.alt = "avatar sabira";
-        avatar_sabira.className = 'avatar_img';
+        avatar_sabira.className = `w-[120px] h-[120px] rounded-full object-cover mb-2 self-center`;
 
-        const sabira = document.createElement('a');
-        sabira.className = "avatar_name";
-        sabira.textContent = "Sabira Makhamatkaiym kyzy";
+        const sabira = document.createElement('p');
+        sabira.className = `text-[8px] font-bold text-center w-full break-words mb-1 mt-2`;
+        sabira.textContent = "Makhamatkaiym kyzy Sabira";
 
-        const role_sabira = document.createElement('a');
-        role_sabira.className = "avatar_role";
+        const role_sabira = document.createElement('p');
+        role_sabira.className = `text-[8px] text-left w-full`;
         role_sabira.textContent = "Frontend developer";
 
         secondAvatar.appendChild(avatar_sabira);
         secondAvatar.appendChild(sabira);
         secondAvatar.appendChild(role_sabira);
 
-        //third avatar
+        // Third avatar
         const thirdAvatar = document.createElement("div");
-        thirdAvatar.className = "avatar_card";
+        thirdAvatar.className = `flex flex-col items-start 
+            p-4 border-2 border-red-500 rounded-[10px]
+            w-[180px] h-[200px]`;
 
         const avatar_dina = document.createElement('img');
         avatar_dina.src = "/assets/avatar/dina.png";
-        avatar_dina.alt = "avatar sabira";
-        avatar_dina.className = 'avatar_img';
+        avatar_dina.alt = "avatar dina";
+        avatar_dina.className = `w-[120px] h-[120px] rounded-full object-cover mb-2 self-center`;
 
-        const dina = document.createElement('a');
-        dina.className = "avatar_name";
+        const dina = document.createElement('p');
+        dina.className = `text-[8px] font-bold text-left w-full mb-1 mt-2`;
         dina.textContent = "Dina";
 
-        const role_dina = document.createElement('a');
-        role_dina.className = "avatar_role";
+        const role_dina = document.createElement('p');
+        role_dina.className = `text-[8px] text-left w-full `;
         role_dina.textContent = "Backend developer";
 
         thirdAvatar.appendChild(avatar_dina);
         thirdAvatar.appendChild(dina);
         thirdAvatar.appendChild(role_dina);
-        
-        //fourth avatar
+
+        // Fourth avatar
         const fourthAvatar = document.createElement("div");
-        fourthAvatar.className = "avatar_card";
+        fourthAvatar.className =  `flex flex-col items-start 
+            p-4 border-2 border-red-500 rounded-[10px]
+            w-[180px] h-[200px]`;
 
         const avatar_rawan = document.createElement('img');
         avatar_rawan.src = "/assets/avatar/rawan.png";
-        avatar_rawan.alt = "avatar sabira";
-        avatar_rawan.className = 'avatar_img';
+        avatar_rawan.alt = "avatar rawan";
+        avatar_rawan.className = `w-[120px] h-[120px] rounded-full object-cover mb-2 self-center`;
 
-        const rawan = document.createElement('a');
-        rawan.className = "avatar_name";
+        const rawan = document.createElement('p');
+        rawan.className = `text-[8px] font-bold text-left w-full mb-1 mt-2`;
         rawan.textContent = "Rawan";
 
-        const role_rawan = document.createElement('a');
-        role_rawan.className = "avatar_role";
+        const role_rawan = document.createElement('p');
+        role_rawan.className = `text-[8px] text-left w-full`;
         role_rawan.textContent = "Backend developer";
 
         fourthAvatar.appendChild(avatar_rawan);
@@ -133,19 +155,12 @@ export class Creators implements IComponent {
 
         image_section.appendChild(avatarsGrid);
 
-        container.appendChild(text_section);
-        container.appendChild(image_section);
-        return container;
-    }
+        subContainer.appendChild(text_section);
+        subContainer.appendChild(image_section);
 
-    private loadPageStyles(): void {
-        if (document.getElementById('creators-styles')) return;
-        
-        const link = document.createElement('link');
-        link.id = 'creators-styles';
-        link.rel = 'stylesheet';
-        link.href = '/styles/Creators.css';
-        document.head.appendChild(link);
+        //Add to the main container
+        container.appendChild(subContainer);
+        return container;
     }
 
 }
