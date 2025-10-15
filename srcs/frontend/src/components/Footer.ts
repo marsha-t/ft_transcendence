0e95ec6c9c0ac625044892d59590309190eda8ec
mk-sabira <buusabira.makhamatkayimkyzy.ch22@gmail.com>
2025-10-15 17:59:44 +0400

    added footer style

--- srcs/frontend/src/components/Footer.ts
+++ srcs/frontend/src/components/Footer.ts
@@ -3,9 +3,19 @@ import { IComponent } from "./IComponent";
 export class Footer implements IComponent {
     public render(): HTMLElement {
         const footer = document.createElement('footer');
+        footer.className = `bg-color-yellow pt-3`;
+
+
+        const subFooter = document.createElement('div');
+        subFooter.className = `bg-background py-6 rounded-[16px]
+            shadow-[0_-4px_4px_rgba(0,0,0,0.50)]`;
+
         const content = document.createElement('p');
         content.innerHTML = `© ${new Date().getFullYear()} Transcendence. All rights reserved.`;
-        footer.appendChild(content);
+        
+        subFooter.appendChild(content);
+
+        footer.appendChild(subFooter);
 
         return footer;
     }
