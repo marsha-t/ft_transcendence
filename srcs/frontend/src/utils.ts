10ee30f62f6286749bbf844eac62e372fdbd588b
Marsha <151203981+marsha-t@users.noreply.github.com>
2025-10-21 18:56:52 +0000

    add listeners to tournament match and game for navigation away from page

--- srcs/frontend/src/utils.ts
+++ srcs/frontend/src/utils.ts
@@ -2,13 +2,40 @@
 // Routing
 //--------------------------
 export function navigate(path: string, state: any = {}) {
-	if (window.location.pathname === path) return;
-	window.history.pushState(state, '', path);
-	window.dispatchEvent(new PopStateEvent('popstate'));
+  if (window.location.pathname === path) return;
+  window.history.pushState(state, "", path);
+  window.dispatchEvent(new PopStateEvent("popstate"));
 }
 
-export function isTournamentFlow(pathname: string): boolean {
-  return pathname.startsWith("/tournament/");
+export function enableLeaveWarning(
+  message = "Leaving will end the current match."
+) {
+  const beforeUnload = (e: BeforeUnloadEvent) => {
+    e.preventDefault();
+    e.returnValue = message;
+  };
+
+  const handlePopState = (e: PopStateEvent) => {
+    e.stopImmediatePropagation(); // prevent Router's popstate listener from running
+
+    const confirmLeave = confirm(message);
+    if (confirmLeave) {
+      cleanup();
+      window.dispatchEvent(new PopStateEvent("popstate"));
+    } else {
+      history.pushState({}, "", window.location.pathname);
+    }
+  };
+
+  window.addEventListener("beforeunload", beforeUnload);
+  window.addEventListener("popstate", handlePopState, true);
+
+  const cleanup = () => {
+    window.removeEventListener("beforeunload", beforeUnload);
+    window.removeEventListener("popstate", handlePopState, true);
+  };
+
+  return cleanup;
 }
 
 //--------------------------
