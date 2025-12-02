85e5f3403c3dd641fa71505d794fe78245c93f14
mk-sabira <buusabira.makhamatkayimkyzy.ch22@gmail.com>
2025-12-02 12:28:05 +0400

    added files for websocket services and its types

--- srcs/frontend/src/services/ApiServices.ts
+++ srcs/frontend/src/services/ApiServices.ts
@@ -1,13 +1,11 @@
 import { AuthServices } from './auth/AuthServices.js';
 import { ProfileServices } from './profile/ProfileServices.js';
-// import { UserService } from './user/UserService';
 import { GameService } from './game/GameService.js';
 import { TournamentService } from './tournament/TournamentService.js';
 import { DashboardService } from './dashboard/DashboardService.js';
 
 export const apiServices = {
     auth: new AuthServices(),
-    // user: new UserService(),
     profile : new ProfileServices(),
     game: new GameService(),
     tournament: new TournamentService(),
