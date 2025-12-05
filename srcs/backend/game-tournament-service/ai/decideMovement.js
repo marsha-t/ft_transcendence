7650f5426e1ee29bbfd333c30b8491511360fe4b
Marsha <151203981+marsha-t@users.noreply.github.com>
2025-12-05 10:12:24 +0000

    adjust AI to return direction

--- srcs/backend/game-tournament-service/ai/decideMovement.js
+++ srcs/backend/game-tournament-service/ai/decideMovement.js
@@ -1,19 +1,17 @@
-function decideMovement({ aiPaddleZ, zIntercept, arena }) {
+export default function decideMovement({ aiPaddleZ, zIntercept, arena }) {
 
 	// Add tolerance to prevent paddle from jittering around target when paddle is 'almost' aligned
 	const tolerance = 0.25;
 	const paddleBoundaryBuffer = 0.3;
 
-	if (aiPaddleZ <= arena.zMin + paddleBoundaryBuffer) return "DOWN";
-	if (aiPaddleZ >= arena.zMax - paddleBoundaryBuffer) return "UP";
+	if (aiPaddleZ <= arena.zMin + paddleBoundaryBuffer) return "UP";
+	if (aiPaddleZ >= arena.zMax - paddleBoundaryBuffer) return "DOWN";
 
 	if (aiPaddleZ < zIntercept - tolerance) {
-		return "DOWN";
+		return "UP";
 	}
 	if (aiPaddleZ > zIntercept + tolerance) {
-		return "UP";
+		return "DOWN";
 	}
 	return "NONE";
 };
-
-module.exports = decideMovement;
