1c7ac1c0a778e72b083ac813158185ac696911fd
Marsha <151203981+marsha-t@users.noreply.github.com>
2025-12-26 12:15:39 +0000

    add reaction delay to AI Brain

--- srcs/backend/game-tournament-service/ai/aiBrain.js
+++ srcs/backend/game-tournament-service/ai/aiBrain.js
@@ -5,10 +5,12 @@ export default class AIBrain {
     /*
         - lastUpdateTime: to enforce 'thinking' once per second
         - lastUpdateTime: to cache predictions in between 'thoughts'
+        - predictionReadyAt: to add reaction delay for AI to decide movement after prediction
     */
     constructor() {
         this.lastUpdateTime = 0;
         this.lastPrediction = null;
+        this.predictionReadyAt = 0;
     }
 
     // Orchestrate how AI decides
@@ -16,7 +18,11 @@ export default class AIBrain {
         - Check at least 1 second has passed since last prediction
         - If so, 
             - call predictBallIntercept (intersection between ball and AI paddle) and cache prediction in lastPrediction
+            - add 250 milliseconds (reaction delay) before AI is allowed to act on prediction
+        On every frame: 
             - call decideMovement to generate AI's move
+                - If reaction delay has elapsed, pass cached prediction to decideMovement
+                - If not, withold prediction so AI hesitates
     */
     decide(snapshot) {
         const now = Date.now();
@@ -32,14 +38,17 @@ export default class AIBrain {
                 oppPaddle: snapshot.oppPaddle,
                 constants: snapshot.constants,
             });
+
             this.lastUpdateTime = now;
+            this.predictionReadyAt = now + 250;
         }
+        const canAct = Date.now() >= this.predictionReadyAt;
 
         return decideMovement({
             aiPaddleZ: snapshot.aiPaddle.z,
-            zIntercept: this.lastPrediction.zIntercept,
+            zIntercept: canAct ? this.lastPrediction.zIntercept : null,
             arena: snapshot.arena,
-            hasIntercept: this.lastPrediction.willReach,
+            hasIntercept: canAct && this.lastPrediction.willReach,
         });
     }
 }
