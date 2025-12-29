620e31e3af2286ad508ce5064deb3a3f86ae8a53
Rawan Al Jaberi <154672950+raljaberi@users.noreply.github.com>
2025-12-29 09:51:25 +0000

    Fixed error handeling in Profile services

--- srcs/backend/profile-service/server.js
+++ srcs/backend/profile-service/server.js
@@ -64,12 +64,33 @@ app.decorate('authenticate', async (request, reply) => {
 app.get('/health', async (request, reply) => {
   return { 
     status: 'ok', 
-    service: 'auth-service'  // Change name for each service
+    service: 'profile-service'  // Change name for each service
   };
 });
 
 // Register Auth Routes
 app.register(profileRoutes, { prefix: '/api/profileServ' });
+
+app.setErrorHandler((error, request, reply) => {
+  request.log.error(error);
+
+  // AJV validation error
+  if (error.validation?.length) {
+    return reply.code(400).send({
+      error: {
+        message: error.validation[0].message,
+        code: 'VALIDATION_ERROR',
+      },
+    });
+  }
+
+  return reply.code(error.statusCode || 500).send({
+    error: {
+      message: error.message || 'Internal Server Error',
+      code: error.code || 'INTERNAL_ERROR',
+    },
+  });
+});
 
 // Start server
 const PORT = process.env.PROFILE_SERVICE_PORT || 5002;
